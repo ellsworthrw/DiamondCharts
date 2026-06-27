f968c278ffa6ddf9005e73abdb1a66748ef747c3
Reed Ellsworth <ellsworthr@diamondedge.com>
2026-06-27 10:32:27 -0600

    v2.0.0 Kotlin Multiplatform release

---
+++ sampleApp/iosApp/iosApp/ContentView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import ComposeApp
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView()
+            .ignoresSafeArea(.all) // Compose has its own safe-area / keyboard handling
+    }
+}
